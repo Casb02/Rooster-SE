7903060c6d031294bdbf652973983b0b7de0d7f7
Cas Bookamp <casb0002@gmail.com>
2022-02-07 14:19:19 +0100

    Init

---
+++ src/components/Home/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {defineProps} from "vue";
+
+defineProps({
+  title: {
+    type: String,
+    default: 'Header'
+  },
+  subtitle: {
+    type: String,
+    default: 'Default subtitle'
+  }
+});
+
+</script>
+
+<template>
+
+  <div class="header">
+    <h1>Header</h1>
+    <h1>{{ title }}</h1> <br>
+    <h2>{{ subtitle }}</h2>
+  </div>
+
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "../components/Home/Header.vue";
+</script>
+
+<template>
+  <h1 class="underline decoration-4">Home</h1>
+  <Header title="Tombi" subtitle="Pimfra"></Header>
+
+</template>
+
+
+<style scoped>
+
+</style>
