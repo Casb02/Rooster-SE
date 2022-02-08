1bcb83a4344f450cc55e39a1f01e266cf1ccff1a
Cas Bookamp <casb0002@gmail.com>
2022-02-08 20:25:14 +0100

    + Update to TW buttun

--- src/components/UI/Button.vue
+++ src/components/UI/Button.vue
@@ -1,25 +1,12 @@
 <script setup>
-import {defineProps} from "vue";
-defineProps({
-  color: {
-    type: String,
-    default: "#4F46E5"
-  },
-})
 
 </script>
 
 <template>
-  <button class="btn-color text-white font-bold py-2 px-4 rounded">
+  <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
     <slot/>
   </button>
 </template>
 
 <style lang="scss" scoped>
-$bg-color: v-bind(color);
-
-.btn-color {
-  background-color: $bg-color;
-}
-
 </style>
