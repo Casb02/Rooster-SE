943dc14f1c251f9f8d81bbb9f6d6e1aa3b5d65b6
Cas Bookamp <casb0002@gmail.com>
2022-02-08 20:27:32 +0100

    W.I.P. Input.vue

--- src/components/UI/Input.vue
+++ src/components/UI/Input.vue
@@ -1,6 +1,10 @@
 <script setup>
 import {defineProps} from "vue";
 defineProps({
+  name: {
+    type: String,
+    default: "btn"
+  },
   type: {
     type: String,
     default: "text"
@@ -13,13 +17,14 @@ defineProps({
 </script>
 
 <template>
-    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-      <slot/>
+  <div>
+    <label :for="name" class="block text-sm font-bold text-gray-700">
+      <slot></slot>
     </label>
-    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-           id="grid-last-name"
-           :type="type"
-           :placeholder="placeholder">
+    <div class="mt-1">
+      <input :type="type" :name="name" :id="name" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" :placeholder="placeholder" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
