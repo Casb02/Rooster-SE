cd9e1b33a4f76e9b74deb814536dc9984f351a3d
Cas Bookamp <casb0002@gmail.com>
2022-02-12 23:33:54 +0100

    Added bootstrap test to nav and fixed footer position

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,31 @@ import NavLink from "./components/UI/Navigation/NavLink.vue";
 </script>
 
 <template>
-  <div class="main-content">
-    <navbar brand-name="RoosterSE">
-      <nav-link to="/">Home</nav-link>
-      <nav-link to="/agenda">Agenda</nav-link>
-    </navbar>
+  <div class="site-content">
+    <div class="main-content">
+      <navbar brand-name="RoosterSE">
+        <nav-link to="/">Home</nav-link>
+        <nav-link to="/agenda">Agenda</nav-link>
+        <nav-link to="/bs-test">Bootstrap Test</nav-link>
+      </navbar>
 
-    <router-view class="min"/>
+      <router-view/>
+    </div>
+
     <Footer class="footer"/>
   </div>
 
 </template>
 
 <style lang="scss">
-.main-content {
+
+.site-content {
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+}
+.main-content {
+  margin-bottom: 5rem;
 }
 
 .footer {
