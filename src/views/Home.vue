281aa197048291b9b5883684e8f3711c5df459ec
Cas Bookamp <casb0002@gmail.com>
2022-02-12 23:35:18 +0100

    Remove design test from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,24 +6,6 @@ import Header from "../components/Home/Header.vue";
 <template>
 
 
-  <div class="container-fluid">
-    <h1>Rooster Color pallet</h1>
-    <a href="" class="btn btn-brand-1 m-3 ml-0">Knop Kleur 1</a>
-    <a href="" class="btn btn-brand-2 m-3">Knop Kleur 2</a>
-    <a href="" class="btn btn-brand-3 m-3">Knop Kleur 3</a>
-    <a href="" class="btn btn-brand-4 m-3">Knop Kleur 4</a>
-    <a href="" class="btn btn-success m-3">Akkoord</a>
-    <a href="" class="btn btn-danger m-3">Verwijder</a>
-    <div class="alert alert-brand-1">🛈 Dit is een test melding voor kleuren palet</div>
-    <div class="alert alert-brand-2">🛈 Dit is een test melding voor kleuren palet</div>
-    <div class="alert alert-brand-3">🛈 Dit is een test melding voor kleuren palet</div>
-    <div class="alert alert-brand-4">🛈 Dit is een test melding voor kleuren palet</div>
-    <div class="alert alert-brand-4">🛈 Dit is een test melding voor kleuren palet</div>
-    <div class="alert alert-success">🛈 Deze is gelukt :)</div>
-    <div class="alert alert-danger">🛈 Deze is mislukt :(</div>
-    <div class="bg-primary" style="height: 50px; width: 100%"></div>
-  </div>
-
 </template>
 
 
