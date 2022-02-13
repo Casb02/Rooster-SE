cd3b9744603d7a8959d105f75e83a5f29d950399
Cas Bookamp <casb0002@gmail.com>
2022-02-13 14:30:28 +0100

    Fix netlify 2.0

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/scss/main.scss'
-import '../node_modules/bootstrap/dist/js/bootstrap.js'
+// import '../node_modules/bootstrap/dist/js/bootstrap.js'
 
 const app = createApp(App);
 app.use(router);
