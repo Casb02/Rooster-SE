b95d2e1bfe65d2711c376bdbd27d121378e8400a
Cas Bookamp <casb0002@gmail.com>
2022-02-15 09:29:50 +0100

    Update component using card-center

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Login from '../../components/auth/Login.vue';
+import CardCenter from "../../components/ui/elements/CardCenter.vue";
+</script>
+
+<template>
+  <card-center breakpoint="col-sm-10 col-md-9 col-lg-8 col-xl-6">
+    <login/>
+  </card-center>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/auth/Register.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Register from '../../components/auth/Register.vue'
+import CardCenter from "../../components/ui/elements/CardCenter.vue";
+</script>
+
+<template>
+  <card-center breakpoint="col-sm-10 col-md-9 col-lg-8 col-xl-6">
+    <register/>
+  </card-center>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
