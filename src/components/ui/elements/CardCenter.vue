a470371ac6863dcaebfa48798338ff422d436d44
Cas Bookamp <casb0002@gmail.com>
2022-02-15 09:27:53 +0100

    Add card components for input cards

---
+++ src/components/ui/elements/CardCenter.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {defineProps} from "vue";
+
+defineProps({
+  breakpoint: {
+    type: String,
+    default: 'col-sm-10 col-md-9 col-lg-8 col-xl-6'
+  },
+})
+</script>
+
+<template>
+  <div class="container-fluid bg-login d-flex flex-column">
+    <div class="row justify-content-center align-items-center flex-grow-1">
+      <div class="row justify-content-center align-items-center flex-grow-1">
+        <div :class="breakpoint">
+          <slot/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
