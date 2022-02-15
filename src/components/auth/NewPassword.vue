b67602c9f07d6de12e7cb4f79c6bd446c0b61dd2
Cas Bookamp <casb0002@gmail.com>
2022-02-15 19:39:02 +0100

    Fixed folder uppercase bug

---
+++ src/components/auth/NewPassword.vue
@@ -0,0 +1,36 @@
+<script setup>
+import {ref} from "vue";
+const newPassword = ref("");
+const newPasswordRepeat = ref("");
+</script>
+
+<template>
+  <div class="login-form ">
+    <div class="card">
+      <h4 class="card-header">Maak nieuw wachtwoord</h4>
+      <div class="card-body">
+        <form>
+          <div class="mb-3">
+            <label for="pass1Input" class="form-label">Nieuw wachtwoord</label>
+            <input type="password" class="form-control" id="pass1Input" v-model="newPassword">
+          </div>
+          <div class="mb-3">
+            <label for="pass2Input" class="form-label">Herhaal wachtwoord</label>
+            <input type="password" class="form-control" id="pass2Input" v-model="newPasswordRepeat">
+          </div>
+          <button type="submit" class="btn btn-primary">Verzenden</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.login-form {
+  max-width: 22rem;
+  margin: 0 auto;
+}
+.tz-small {
+  font-size: 0.7rem;
+}
+</style>
---
+++ src/components/auth/ResetPassword.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from "vue";
+const email = ref('');
+</script>
+
+<template>
+  <div class="login-form ">
+    <div class="card">
+      <h4 class="card-header">Wachtwoord herstellen</h4>
+      <div class="card-body">
+        <form>
+          <div class="mb-3">
+            <label for="emailInput" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="emailInput" v-model="email">
+          </div>
+          <button type="submit" class="btn btn-primary">Verstuur</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.login-form {
+  max-width: 22rem;
+  margin: 0 auto;
+}
+.tz-small {
+  font-size: 0.7rem;
+}
+</style>
