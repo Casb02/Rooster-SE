1f6219093c43440c734ad0e10675afc0c8ad0c81
Cas Bookamp <casb0002@gmail.com>
2022-02-16 21:57:19 +0100

    Type Scripto!

--- src/views/auth/NewPassword.vue
+++ src/views/auth/NewPassword.vue
@@ -7,7 +7,6 @@ import CardCenter from "../../components/ui/elements/CardCenter.vue";
   <card-center breakpoint="col-sm-10 col-md-9 col-lg-8 col-xl-6">
     <new-password/>
   </card-center>
-
 </template>
 
 <style scoped>
