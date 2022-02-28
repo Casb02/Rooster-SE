cfcf9e2e4950df06883e9ec1bd23e1ba5ccc71fc
Cas Bookamp <casb0002@gmail.com>
2022-03-01 00:01:23 +0100

    Continued development...

--- src/components/ui/calendar/Calendar.vue
+++ src/components/ui/calendar/Calendar.vue
@@ -4,10 +4,11 @@ import FullCalendar from '@fullcalendar/vue3';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import nlLocale from '@fullcalendar/core/locales/nl';
+import iCalendarPlugin from '@fullcalendar/icalendar';
 import {reactive} from "vue";
 
 const calendarOptions = reactive({
-  plugins: [ timeGridPlugin, dayGridPlugin ],
+  plugins: [ timeGridPlugin, dayGridPlugin, iCalendarPlugin ],
   initialView: 'timeGridWeek',
   locale: nlLocale,
   height: 'auto',
@@ -19,20 +20,15 @@ const calendarOptions = reactive({
   },
   slotMinTime: "07:00:00",
   slotMaxTime: "18:30:00",
+  //generate random events between today and 7 days later
+  events: {
+    url: 'https://myx-saxion.xedule.nl/api/InternetCalendar/feed/86e89394-2512-4dea-9e83-50f334fc13f5/33a27fbf-5fa1-467b-9318-a4dca77a7b45/',
+    format: 'ics'
+  }
 })
+
 
 </script>
 <template>
-  <FullCalendar :options="calendarOptions" />
+  <full-calendar ref="cal" :options="calendarOptions" />
 </template>
-
-<style lang="scss">
-.fc-today-button .fc-button .fc-button-primary {
-  background-color: #f5f5f5;
-  color: #000;
-}
-
-.fc-button-group {
-  background: aqua;
-}
-</style>
